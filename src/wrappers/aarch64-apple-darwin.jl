0795bae5bd039ee91ee59cadcf6788ffeb584195
jlbuild <juliabuildbot@gmail.com>
2025-08-20 20:23:58 +0000

    libharu_jll build 2.4.5+0

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -9,7 +9,7 @@ function __init__()
     JLLWrappers.@generate_init_header(Zlib_jll, libpng_jll)
     JLLWrappers.@init_library_product(
         libhpdf,
-        "lib/libhpdf.2.4.4.dylib",
+        "lib/libhpdf.2.4.5.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
